a983c46d4e8575c2345b1fc2ed4353cb3382b0a6
kr8gz <47776344+kr8gz@users.noreply.github.com>
2023-02-03 10:21:18 +0100

    bom

--- index.js
+++ index.js
@@ -1,71 +1,20 @@
-const greetings = [
-    "Hey!", 
-    "Hiya!", 
-    "Hello!", 
-    "Hé!", 
-    "Hallo!", 
-    "Ahoj!", 
-    "Salut!",
-    "Hei!",
-    "Hej!",
-    "¡Hola!",
-    "Servus!",
-];
+const links = document.getElementById("links")
 
-const links = document.getElementById("links");
-const about = document.getElementsByClassName("link")[0];
-const games = document.getElementsByClassName("link")[1];
-const articleAboutMe = document.getElementById("aboutme");
-const greeting = document.getElementById("greeting");
-const articleGames = document.getElementById("games");
-const arrow = document.getElementById("back");
+for (var link of links.getElementsByTagName("a")) {
+    link.addEventListener("mouseover", event => {
+        var a = document.createElement("a")
+        a.href = event.target.getAttribute("href")
+        a.style.left = "0px"
+        a.style.top = "0px"
 
-const random = max => Math.floor(Math.random() * max);
+        var img = document.createElement("img")
+        img.src = "/media/explosion.gif?reload=" + Math.random()
+        img.style.left = event.clientX - 120 + "px"
+        img.style.top = event.clientY - 180 + "px"
 
-const randomGreeting = () => greeting.innerHTML = greetings[random(greetings.length)];
-
-const reset = () => {
-    about.dataset.focus = "none";
-    games.dataset.focus = "none";
-};
-
-const disabled = () => links.dataset.moveleft == "yes";
-
-about.addEventListener("mouseover", event => {
-    if (disabled()) return;
-    event.target.dataset.focus = "yes";
-    games.dataset.focus = "no";
-});
-
-games.addEventListener("mouseover", event => {
-    if (disabled()) return;
-    event.target.dataset.focus = "yes";
-    about.dataset.focus = "no";
-});
-
-
-about.addEventListener("click", event => {
-    if (disabled()) return;
-    links.dataset.moveleft = "yes";
-    articleAboutMe.dataset.visible = "yes";
-    arrow.dataset.visible = "yes";
-    randomGreeting();
-});
-
-games.addEventListener("click", event => {
-    if (disabled()) return;
-    links.dataset.moveleft = "yes";
-    articleGames.dataset.visible = "yes";
-    arrow.dataset.visible = "yes";
-});
-
-arrow.addEventListener("click", event => {
-    articleAboutMe.dataset.visible = "no";
-    articleGames.dataset.visible = "no";
-    arrow.dataset.visible = "no";
-    links.dataset.moveleft = "no";
-});
-
-
-games.addEventListener("mouseleave", reset);
-about.addEventListener("mouseleave", reset);
+        a.appendChild(img)
+        links.appendChild(a)
+        
+        setTimeout(() => links.removeChild(a), 1300)
+    })
+}
