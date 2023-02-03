a6d80a06c69946d977e7f6ab0da998d16975a229
kr8gz <47776344+kr8gz@users.noreply.github.com>
2023-02-03 22:14:36 +0100

    upgrade

--- index.js
+++ index.js
@@ -5,24 +5,19 @@ const links = document.getElementById("links")
 
 for (var link of links.getElementsByTagName("a")) {
     link.addEventListener("mouseover", event => {
-        var a = document.createElement("a")
-        a.href = event.target.getAttribute("href")
-        a.style.left = "0"
-        a.style.top = "0"
-
         var img = document.createElement("img")
         img.src = "/media/explosion.gif?reload=" + Math.random()
         img.style.left = event.x - 120 + "px"
         img.style.top = event.y - 170 + "px"
 
-        a.appendChild(img)
-        links.appendChild(a)
-        setTimeout(() => links.removeChild(a), 1250)
+        new Audio("/media/boom.mp3").play()
+        links.appendChild(img)
+        setTimeout(() => links.removeChild(img), 1250)
     })
 }
 
-// greetings
-const greetings_list = [
+// greeting
+const greeting_list = [
     "hello :)",
     "hi :D",
     "griaß di",
@@ -38,16 +33,15 @@ const greetings_list = [
     "salut!",
     "buongiorno (puon tschorno)",
 ]
-var last_choice = greetings_list[0]
+var last_choice = greeting_list[0]
 
-const greetings = document.getElementById("greetings")
+const header = document.getElementById("header")
 
-greetings.addEventListener("mousedown", event => {
+header.addEventListener("mousedown", event => {
     var greeting = document.getElementById("greeting")
     var choice
     do {
-        choice = greetings_list[random(greetings_list.length)]
+        choice = greeting_list[random(greeting_list.length)]
     } while (choice == last_choice)
-    greeting.innerHTML = "- " + choice
-    last_choice = choice
+    last_choice = greeting.innerHTML = choice
 })
